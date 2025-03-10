198c8ea364da6b630539b20147ae267596c419fe
Paulo Santiago <p.santiago796@gmail.com>
2025-03-09 20:26:03 -0400

    remove boilerplate

--- server/main.py
+++ server/main.py
@@ -19,33 +19,6 @@ from gui import server_ui
 # import TCP module from connection package
 from connection import TCP
 
-
-# # UI boiler plate
-# def show(event: ValueChangeEventArguments):
-#     name = type(event.sender).__name__
-#     ui.notify(f'{name}: {event.value}')
-
-# ui.button('Button', on_click=lambda: ui.notify('Click'))
-# with ui.row():
-#     ui.checkbox('Checkbox', on_change=show)
-#     ui.switch('Switch', on_change=show)
-# ui.radio(['A', 'B', 'C'], value='A', on_change=show).props('inline')
-# with ui.row():
-#     ui.input('Text input', on_change=show)
-#     ui.select(['One', 'Two'], value='One', on_change=show)
-# ui.link('And many more...', '/documentation').classes('mt-8')
-
-# ui.label('Server State')
-# with ui.column():
-#     ui.button('Start Server', on_click=lambda: ui.notify('Server On'))
-#     ui.button('Stop Server', on_click=lambda: ui.notify('Server Off'))
-
-# ui.run()
-
-# server_state.ui.run()
-# serverui = server_ui()
-# server_ui.ui.run()
-
 # run TCP connection
 server = TCP()
 
